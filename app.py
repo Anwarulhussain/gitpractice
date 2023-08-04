e4c28e39c10f7fcc5c161fe134686ee7d854b0c6
Anwarul Hussain <786anwarulhussain@gmail.com>
2023-08-04 20:09:02 +0530

    code added

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-### create a simple flask application
-from flask import Flask
-### create a flask app
-app=Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello world"
+from flask import Flask,request,render_template,jsonify
+import json
 
-@app.route('/success/<int:score>')
-def success(score):
-    return "the score is"+str(score)
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return"welcome to the Flask"
+
+@obj.route('/cal',methods=['GET'])
+def math_op():
+    operations=request.json["operation"]
+    
+    
+    
+    
+    
+    
+    num1=request.json["operation1"]
+    num2=request.json["operation2"]
+    if operations=="add":
+           result=num1+num2
+    elif operations=="multiply":
+        result=num1*num2
+    elif operations=="division":
+        result=num1/num2
+    else:
+        result=num1-num2
+    return jsonify(result)
+print(__name__)
+
 
 if __name__=='__main__':
-  app.run()
 
-
-
+    app.run()
